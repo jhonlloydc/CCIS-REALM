4538c2a686af7a388b66223d30479f982d7a1714
Jhon lloyd Clarion <50243861+jhonlloydc@users.noreply.github.com>
2021-11-23 20:13:50 +0800

    v1

--- js/config.js
+++ js/config.js
@@ -3,3 +3,20 @@ var page1 = "Page one";
 var page2 = "Page two";
 
 // Data Tables
+
+const data1 = {
+
+    'first':[
+        {'points':100}
+    ],
+    'second':[
+        {'points':100}
+    ],
+    'third':[
+        {'points':100}
+    ],
+    'fourth':[
+        {'points':100}
+    ]
+
+};
