e2bf7e0751a43637683c0c69dea1c88d13c2bae3
Jhon lloyd Clarion <50243861+jhonlloydc@users.noreply.github.com>
2021-11-23 22:23:54 +0800

    v1

--- js/config.js
+++ js/config.js
@@ -4,9 +4,29 @@ var page2 = "Page two";
 var page3 = "Page three";
 var page4 = "Page four";
 
-// Data Tables
+// Data Day 1
 
 const data1 = {
+
+    'first':[
+        {'points':100},
+        {'team':'Bayson Team'}
+    ],
+    'second':[
+        {'points':100}
+    ],
+    'third':[
+        {'points':100}
+    ],
+    'fourth':[
+        {'points':100}
+    ]
+
+};
+
+// Data Day 2
+
+const data2 = {
 
     'first':[
         {'points':100},
