ef2bbbd895f56b83804b84b476222c889a3ebc1d
Jhon lloyd Clarion <50243861+jhonlloydc@users.noreply.github.com>
2021-11-25 22:24:06 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,49 @@
+// Header TITLE PAGE
+
+var pageheader = "Welcome to CCIS Page";
+
+// Set Page Titles
+var page1 = "Page one";
+var page2 = "Page two";
+var page3 = "Page three";
+var page4 = "Page four";
+
+// Data Day 1
+
+const data1 = {
+
+    'first':[
+        {'points':100},
+        {'team':'Bayson Team'}
+    ],
+    'second':[
+        {'points':100}
+    ],
+    'third':[
+        {'points':100}
+    ],
+    'fourth':[
+        {'points':100}
+    ]
+
+};
+
+// Data Day 2
+
+const data2 = {
+
+    'first':[
+        {'points':100},
+        {'team':'Bayson Team'}
+    ],
+    'second':[
+        {'points':100}
+    ],
+    'third':[
+        {'points':100}
+    ],
+    'fourth':[
+        {'points':100}
+    ]
+
+};
